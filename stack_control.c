13ca741e5a13fedb238382621d3ea0eb1bde0f4a
Tamara Mkrtchyan <tmkrtchy@c1r3s4.42yerevan.am>
2021-06-08 20:22:17 +0400

    stack actions

---
+++ stack_control.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filling.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmkrtchy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/03 20:05:39 by tmkrtchy          #+#    #+#             */
+/*   Updated: 2021/06/03 20:05:57 by tmkrtchy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack	*ft_create_stack(int *buff, int count)
+{
+	int		i;
+	t_stack	*new_list;
+	t_stack	*first;
+
+	i = 0;
+	if (!(new_list = (t_stack *)malloc(sizeof(t_stack))))
+		exit(1);
+	first = new_list;
+	while (i < count)
+	{
+		if (i < count - 1)
+		{
+			if (!(new_list->next = (t_stack *)malloc(sizeof(t_stack))))
+				exit(1);
+		}
+		new_list->data = buff[i];
+		if (i == (count - 1))
+			new_list->next = NULL;
+		else
+			new_list = new_list->next;
+		i++;
+	}
+	return (first);
+}
+
+void	ft_init_stack(t_data *new, t_stacks *stacks)
+{
+	stacks->a = ft_create_stack(new->a, new->count_element);
+	stacks->b = NULL;
+}
+
+void	ft_free_stack(t_stacks *stacks)
+{
+	int		i;
+	t_stack	*buff;
+
+	i = 0;
+	while (i < stacks->count_a)
+	{
+		buff = stacks->a;
+		stacks->a = stacks->a->next;
+		free(buff);
+		i++;
+	}
+	i = 0;
+	while (i < stacks->count_b)
+	{
+		buff = stacks->b;
+		stacks->b = stacks->b->next;
+		free(buff);
+		i++;
+	}
+}
+
+void	ft_null(t_stacks *stacks, t_data *new)
+{
+	new->count_element = 0;
+	stacks->count_a = 0;
+	stacks->count_b = 0;
+	stacks->min = 0;
+	stacks->max = 0;
+	stacks->med = 0;
+	stacks->a = NULL;
+	stacks->b = NULL;
+}
